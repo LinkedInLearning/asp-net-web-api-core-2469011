ba23ff79d8b6a542d3d13b2ab0557389b6aa1939
Christian Wenz <christian@wenz.org>
2022-02-11 11:30:45 +0100

    06_05e

--- HPlusSport/HPlusSport.API/Program.cs
+++ HPlusSport/HPlusSport.API/Program.cs
@@ -35,6 +35,16 @@ builder.Services.AddDbContext<ShopContext>(options =>
     options.UseInMemoryDatabase("Shop");
 });
 
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(builder =>
+    {
+        builder
+            .WithOrigins("https://localhost:7297")
+            .WithHeaders("X-API-Version");
+    });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -50,6 +60,8 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+app.UseCors();
 
 app.MapControllers();
 
