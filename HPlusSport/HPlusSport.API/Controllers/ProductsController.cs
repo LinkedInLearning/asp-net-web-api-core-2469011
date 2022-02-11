ac9343b76b4af875936d5955548db46a20bde312
Christian Wenz <christian@wenz.org>
2022-02-11 11:22:45 +0100

    03_05e

--- HPlusSport/HPlusSport.API/Controllers/ProductsController.cs
+++ HPlusSport/HPlusSport.API/Controllers/ProductsController.cs
@@ -47,5 +47,33 @@ namespace HPlusSport.API.Controllers
                 new { id = product.Id },
                 product);
         }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> PutProduct(int id, [FromBody]Product product)
+        {
+            if (id != product.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(product).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!_context.Products.Any(p => p.Id == id))
+                {
+                    return NotFound();
+                } else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
     }
 }
