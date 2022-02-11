9eac0420ca19e7de992dc40d7128a00dab5f6f4f
Christian Wenz <christian@wenz.org>
2022-02-11 11:23:10 +0100

    03_06e

--- HPlusSport/HPlusSport.API/Controllers/ProductsController.cs
+++ HPlusSport/HPlusSport.API/Controllers/ProductsController.cs
@@ -75,5 +75,20 @@ namespace HPlusSport.API.Controllers
 
             return NoContent();
         }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Product>> DeleteProduct(int id)
+        {
+            var product = await _context.Products.FindAsync(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            _context.Products.Remove(product);
+            await _context.SaveChangesAsync();
+
+            return product;
+        }
     }
 }
